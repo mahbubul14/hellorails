3bf5c7df35944b639da6673fefc14d38033cd4d0
mahbubul14 <mahbub21alam2017@gmail.com>
2022-03-21 21:36:10 +0600

    Delete all files

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root 'pages#hello'
-
-  get 'pages/hello'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
